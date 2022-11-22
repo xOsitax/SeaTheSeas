74515e3529b9a844f002fddb66405a44e4ed39c7
stumplaura <stumpl@kean.edu>
2022-11-22 11:23:41 -0500

    Update index-1.php
    The Arena Accessibility north

--- Front End/index-1.php
+++ Front End/index-1.php
@@ -679,7 +679,7 @@ if (isset($_SESSION["user_id"])) {
                                         <div class="trend-last-main">
                                             <p class="mb-0 trend-para">The Grand Canal is the main waterway of Venice. The Canal is lined with palaces and spanned by the Rialto Bridge.</p>
                                             <div class="trend-last d-flex align-items-center justify-content-between">
-                                                <p class="mb-0 white d-flex align-items-center"><img src="images/reviewer/1.jpg" class="d-author mr-2" alt=""> Thu Astudillo</p>
+                                                <p class="mb-0 white d-flex align-items-center"><img src="images/reviewer/1.jpg" class="d-author mr-2" alt=""> Price: £30/adult</p>
                                                 <div class="trend-price">
                                                     <p class="price white mb-0"><a href="#"><i class="flaticon-like white"></i></a></p>
                                                 </div>
@@ -705,13 +705,13 @@ if (isset($_SESSION["user_id"])) {
                                                 </div>
                                                 <span class="ml-2">38 Reviews</span>
                                             </div>
-                                            <h4><a href="#"> Family Tour</a></h4>
+                                            <h4><a href="#"> The Arena</a></h4>
                                             <p class="mb-0"><i class="fa fa-eye mr-1"></i> 615 Visiting Places <i class="fa fa-map-marker mr-1 ml-3"></i> Italy</p>
                                         </div>
                                         <div class="trend-last-main">
-                                            <p class="mb-0 trend-para">A wonderful little cottage right on the seashore - perfect for exploring.</p>
+                                            <p class="mb-0 trend-para">This is a Roman amphitheatre in Piazza Bra, built in 30 AD. It is still in use and known world wide for the huge opera performances given there.</p>
                                             <div class="trend-last d-flex align-items-center justify-content-between">
-                                                <p class="mb-0 white d-flex align-items-center"><img src="images/reviewer/2.jpg" class="d-author mr-2" alt=""> Thu Astudillo</p>
+                                                <p class="mb-0 white d-flex align-items-center"><img src="images/reviewer/2.jpg" class="d-author mr-2" alt=""> Price: £10/person</p>
                                                 <div class="trend-price">
                                                     <p class="price white mb-0"><a href="#"><i class="flaticon-like white"></i></a></p>
                                                 </div>
