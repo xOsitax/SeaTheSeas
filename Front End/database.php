6b68b5a3203726a051eaf5013359e510b14fe39e
mstump8 <stumpm@kean.edu>
2022-11-22 16:32:56 -0500

    Update to North Blog quote
    Inputted a quote for the quote box. Line 148-149

--- Front End/database.php
+++ Front End/database.php
@@ -9,7 +9,7 @@ $mysqli = new mysqli(hostname: $host,
                      username: $username,
                      password: $password,
                      database: $dbname);
-                     
+
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
 }
